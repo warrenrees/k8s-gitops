ea44cca9ca3bbce574925576d0fd1960c9ad1b7c
Warren Rees <warren@rees.ca>
2022-01-06 06:53:57 +0000

    Additional secondary control plane bootstrapping

--- clusters/bootstrap/nodes/03_kubernetes_remaining_control.sh
+++ clusters/bootstrap/nodes/03_kubernetes_remaining_control.sh
@@ -2,4 +2,11 @@
 # do things
 # copy certs
 # run kubeadm --join
+sudo kubeadm config images pull --cri-socket /run/containerd/containerd.sock
+sudo kubeadm join k8s-cluster.rees.ca:6443 --token <token> \
+        --discovery-token-ca-cert-hash <ca-cert-hash> \
+        --control-plane --certificate-key <cert-key>
 
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
