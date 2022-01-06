dc57e69d77a5d155d766370bf3365c2c0b0da138
Warren Rees <warren@rees.ca>
2022-01-06 06:22:06 +0000

    Updates to node bootstraps

--- clusters/bootstrap/nodes/02_kubernetes_first_control.sh
+++ clusters/bootstrap/nodes/02_kubernetes_first_control.sh
@@ -5,3 +5,11 @@ sudo kubeadm init \
   --cri-socket /run/containerd/containerd.sock \
   --upload-certs \
   --control-plane-endpoint="k8s-cluster.rees.ca:6443"
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl create -f tigera-operator.yaml
+kubectl create -f custom-resources.yaml
+kubectl taint nodes --all node-role.kubernetes.io/master-
