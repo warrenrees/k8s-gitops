d5e4edd803d096711c91eee6c9d422b68641fed3
Warren Rees <warren@rees.ca>
2022-12-16 00:15:55 -0500

    updates for k8s 1.26

--- clusters/bootstrap/nodes/01_containerd.sh
+++ clusters/bootstrap/nodes/01_containerd.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # Add Docker repo
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+wget -qO - https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/trusted.gpg.d/docker-archive-keyring.asc
+echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
 
-# Install containerd
+# Install containerd from docker repo
 sudo apt update
-sudo apt install -y containerd
+sudo apt install -y containerd.io
 
 # Configure containerd and start service
 sudo mkdir -p /etc/containerd
 sudo containerd config default>/etc/containerd/config.toml
 
-echo 'add this after: [plugins."io.containerd.grpc.v1.cri".containerd.runetimes.runc.options]'
-echo '   SystemdCgroup = true'
-
-sleep 10
-sudo nano /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 
 # restart containerd
 systemctl restart containerd
@@ -29,6 +25,7 @@ read INTERFACE
 export KVVERSION=$(curl -sL https://api.github.com/repos/kube-vip/kube-vip/releases | jq -r ".[0].name")
 ctr image pull ghcr.io/kube-vip/kube-vip:$KVVERSION
 alias kube-vip="ctr run --rm --net-host ghcr.io/kube-vip/kube-vip:$KVVERSION vip /kube-vip"
-kube-vip manifest pod     --interface $INTERFACE     --address $VIP     --controlplane     --services     --arp     --leaderElection | tee /etc/kubernetes/manifests/kube-vip.yaml
+kube-vip manifest pod --interface $INTERFACE --address $VIP --controlplane --arp --leaderElection | tee /etc/kubernetes/manifests/kube-vip.yaml
+#kube-vip manifest pod     --interface $INTERFACE     --address $VIP     --controlplane     --services     --arp     --leaderElection | tee /etc/kubernetes/manifests/kube-vip.yaml
 
 systemctl enable kubelet
